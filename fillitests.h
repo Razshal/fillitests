eb4d2f9a72cccc90588ba42434b613b78b6ee257
razshal <mfonteni@e3r3p3.42.fr>
2017-11-19 14:19:40 +0100

    init

---
+++ fillitests.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillitests.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/19 14:16:31 by mfonteni          #+#    #+#             */
+/*   Updated: 2017/11/19 14:18:59 by mfonteni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FILLITESTS_H
+#define FILLITESTS_H
+
+#define SUCCESSMSG "\e[32m[Success]\e[39m\n"
+#define PRINTFSUCCESS printf("%s", SUCCESSMSG)
+#define FAILUREMSG "\e[31m[Failure]\e[39m\n"
+#define PRINTFFAILURE printf("%s", FAILUREMSG)
+#define EASYTESTFAIL printf("Echec d'un test de fonctionnement basique\n");
+#define INTMIN "-2147483648"
+#define INTMAX "2147483647"
+
+#endif
