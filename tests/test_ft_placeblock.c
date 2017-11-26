835ca55f448894ab0179b5a6b3ec913ef0ca6af7
Razshal <fontenille.mathieu@gmail.com>
2017-11-26 20:22:26 +0100

    test_placenext refactor

--- tests/test_ft_placeblock.c
+++ tests/test_ft_placeblock.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/21 12:13:31 by mfonteni          #+#    #+#             */
-/*   Updated: 2017/11/24 19:00:22 by mfonteni         ###   ########.fr       */
+/*   Updated: 2017/11/26 11:28:37 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ void test_ft_placeblock(void)
 
 	char **grid = ft_strsplit(".......... .......... .......... .......... .......... .......... .......... .......... .......... ..........", ' ');
 
-	if (ft_placeblock("dlbd", grid, 2, 1) && ft_alphablock(grid, 'A')
-			&& ft_placeblock("drbd", grid, 0, 0) && ft_alphablock(grid, 'B')
-			&& ft_placeblock("rrr", grid, 1, 0) && ft_alphablock(grid, 'C')
+	if (ft_placeblock("dlbd", grid, 1, 1) && ft_alphablock(grid, 'A')
+			&& ft_placeblock("drbd", grid, 1, 2) && ft_alphablock(grid, 'B')
+			&& ft_placeblock("rrr", grid, 0, 0) && ft_alphablock(grid, 'C')
 		)
 		PRINTFSUCCESS;
 	else
--- tests/test_ft_placenext.c
+++ tests/test_ft_placenext.c
@@ -6,29 +6,32 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 18:39:59 by mfonteni          #+#    #+#             */
-/*   Updated: 2017/11/24 19:00:20 by mfonteni         ###   ########.fr       */
+/*   Updated: 2017/11/26 20:21:51 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../fillitests.h"
 
-int *nextpos(char **grid, int max);
+int *nextpos(char **grid, int limit);
+
+static void	tester(char **grid, char *block, int limit)
+{
+	int *array = nextpos(grid, limit);
+
+	if (ft_placeblock(block, grid, array[0], array[1]) && ft_alphablock(grid, 'A'))
+		PRINTFSUCCESS;
+	else
+		PRINTFFAILURE;
+	ft_print_split(grid);
+
+}
 
 void	test_ft_placenext(void)
 {
 	PRINTNAME("test placenext");
-	char **grid = ft_strsplit(".......... .......... .......... .......... .......... .......... .......... .......... .......... ..........", ' ');
-	int *array = nextpos(grid, 10);
-	if (ft_placeblock("rrr", grid, array[0], array[1]) && ft_alphablock(grid, 'A'))
-		PRINTFSUCCESS;
-	else
-		PRINTFFAILURE;
-	ft_print_split(grid);
 
-	int *array2 = nextpos(grid, 10);
-	if (ft_placeblock("rdl", grid, array2[0], array2[1]) && ft_alphablock(grid, 'B'))
-		PRINTFSUCCESS;
-	else
-		PRINTFFAILURE;
-	ft_print_split(grid);
+	char **grid = ft_strsplit("..... ..... ..... ..... .....", ' ');
+
+	tester(grid, "rrr", 5);
+	tester(grid, "rrr", 5);
 }
